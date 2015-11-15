2c2dd38ff945b9615b65dcd2b02f51ab31e1411b
Meagan Sobol <meagan.sobol@unbounce.com>
2015-11-14 17:29:30 -0800

    W5D2 Wed Nov 11

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,10 @@
 # Homepage (Root path)
+helpers do 
+
+  def current_user
+    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+end
+
 get '/' do
   erb :index
 end
@@ -16,12 +22,31 @@ get 'profile' do
 end
 
 post '/login' do
-  redirect '/'
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+  if user.password == password
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/login'
+  end
 end
 
 post 'signup' do 
-  redirect '/'
-end
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+  if user
+    redirect 'signup'
+  else 
+    user = User.create(username: username, password: password)
+    session[:user_id] = user.id
+    redirect '/'
+  end
+ends
 
 post 'signup' do
   redirect '/'
